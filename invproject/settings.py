60d2e3d4f76e98c6f24a366566e79a49f19e9b2d
Faldi767 <Faldi767@gmail.com>
2023-02-23 14:51:33 +0700

    Base config

--- invproject/settings.py
+++ invproject/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from decouple import config, Csv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'widget_tweaks',
+
+    'inventory',
+    'accounts',
 ]
 
 MIDDLEWARE = [
